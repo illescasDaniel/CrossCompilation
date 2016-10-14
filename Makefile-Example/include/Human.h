b1c5c337af958afa45a7adb1eaa8edf73b12138d
Daniel Illescas Romero <illescas.daniel@protonmail.com>
2016-10-14 15:52:28 +0200

    Create Human.h

---
+++ Makefile-Example/include/Human.h
@@ -0,0 +1,15 @@
+#ifndef headerDani_h
+#define headerDani_h
+
+// This is just a test file to compile it from the makefile
+
+class Human {
+
+public:
+	
+	int age = 10;
+	
+	Human() {}
+};
+
+#endif
